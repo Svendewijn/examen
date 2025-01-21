eb848e1512a5233fdca2f9067d7e73d2a5804e83
Sven <sven11042006@gmail.com>
2025-01-21 14:32:26 +0100

    footer, header and styling

---
+++ footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+        <p>&copy; 2025 Your Company. All rights reserved.</p>
+        <div class="social-icons">
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+        </div>
+        <p>
+            <a href="#">Privacy Policy</a>
+            |
+            <a href="#">Terms of Service</a>
+        </p>
+    </div>
--- index.php
+++ index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styling/styling.css">
 </head>
 <body>
-    
+<?php include 'header.php';?>
+<div class="homepage"></div>
+
+    <div class="textbox">
+    <h1>Welcome!</h1>
+    </div>
+
+</div>
+<?php include 'footer.php';?>
 </body>
 </html>
