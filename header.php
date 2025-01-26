839bd5dcbf0b194e15b3065a120d41f46e21b250
Sven <sven11042006@gmail.com>
2025-01-26 20:40:00 +0100

    header

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <li><a href="home.html">Home</a></li>
         <li><a href="about.html">About</a></li>
         <li><a href="services.html">Services</a></li>
-        <li><a href="contact.html">Contact</a></li>
+        <li><a href="login.php">login</a></li>
       </ul>
     </nav>
   </header>
